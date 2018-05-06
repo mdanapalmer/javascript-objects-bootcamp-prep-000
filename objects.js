3f421863bf84067c7a5c8e55e53e309a10ffa0af
Dana Palmer <roiai4@hotmail.com>
2018-05-06 22:10:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,7 @@
 var playlist = {
   'Maaya Sakamoto': 'Garden of Everything'
 }
+
+function updatePlayList(playlist, artistName, songTitle) {
+  return `Object.assign ({}, obj, artistName: SongTitle)`
+}
